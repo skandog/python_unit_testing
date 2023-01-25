98ebc29c3889f889aa1cfc1256e2e914aaa17282
skandog <muumrar@gmail.com>
2023-01-25 10:12:39 +0000

    added unittest.main() method to allow direct running of the file

--- test_calc.py
+++ test_calc.py
@@ -7,3 +7,7 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result = calc.add(10, 5)
         self.assertEqual(result, 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
